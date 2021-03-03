7e5f76a10319b9e679a652fa767f44c19a7bd66a
yogeshbjadhav <yogeshbjadhav10@gmail.com>
2021-03-03 14:04:50 +0530

    second changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AppComponent {
 
-  testdata = "hello from parent"
+  testdata = "hello frosdfsdfsfsffsdm parent"
   
   constructor(){     
   }
